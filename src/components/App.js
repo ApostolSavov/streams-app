686abf3c9f741cf1c74de7f8a393b1d837ffa6dc
Apostol Savov <apostol.savov.97@gmail.com>
2021-04-19 22:27:00 +0300

    Implemented basic OBS streaming functionality to existing stream id

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 import StreamCreate from './streams/StreamCreate.js';
 import StreamEdit from './streams/StreamEdit.js';
 import StreamDelete from './streams/StreamDelete.js';
@@ -14,27 +14,29 @@ function App(props) {
 			<Router history={history}>
 				<div>
 					<Header></Header>
-					<Route path="/" exact component={StreamList}></Route>
-					<Route
-						path="/streams/new"
-						exact
-						component={StreamCreate}
-					></Route>
-					<Route
-						path="/streams/edit/:id"
-						exact
-						component={StreamEdit}
-					></Route>
-					<Route
-						path="/streams/delete/:id"
-						exact
-						component={StreamDelete}
-					></Route>
-					<Route
-						path="/streams/show"
-						exact
-						component={StreamShow}
-					></Route>
+					<Switch>
+						<Route path="/" exact component={StreamList}></Route>
+						<Route
+							path="/streams/new"
+							exact
+							component={StreamCreate}
+						></Route>
+						<Route
+							path="/streams/edit/:id"
+							exact
+							component={StreamEdit}
+						></Route>
+						<Route
+							path="/streams/delete/:id"
+							exact
+							component={StreamDelete}
+						></Route>
+						<Route
+							path="/streams/:id"
+							exact
+							component={StreamShow}
+						></Route>
+					</Switch>
 				</div>
 			</Router>
 		</div>
